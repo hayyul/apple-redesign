12a798c13b1a29bf3a95397104cec2cf8c82d6ae
hayyulshabani <hayyul.shabani@technoperia.com>
2022-11-04 15:59:29 +0100

    1.18.25

---
+++ components/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Landing() {
+  return (
+      <section>
+          <div>
+              <h1>
+                  <span>Powered</span>
+                <span>Powered</span>
+                  <span>link youtubee 1.18.25</span>
+
+              </h1>
+          </div>
+    </section>
+  )
+}
+
+export default Landing
